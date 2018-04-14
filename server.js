b07ee7cb468aae4e8ddb91a18b54eb9cdab51150
Andrew <andrew@ampletorque.com>
2018-04-14 17:44:52 +0000

    add configurable logger

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const connect = require('connect');
+
+function setup(format) {
+	const regexp = /:(\w+)/g;
+
+	return function createLogger(req, res, next) {
+		const str = format.replace(regexp, (match, property) => {
+			return req[property];
+		});
+		console.log(str);
+		next();
+	}
+}
+module.exports = setup;
+
 function logger(req, res, next) {
 	console.log('%s %s', req.method, req.url);
 	next();
 }
+
 function hello(req, res) {
 	res.setHeader('Content-Type', 'text/plain');
 	res.end('hello world');
 }
-connect()
-	.use(logger)
+
+const app = connect()
+	.use(logger(':method :url'))
+        .use(setup(':method :url'))
 	.use(hello)
 	.listen(3000);
 
