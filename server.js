68f1806404b09a5b186a255c5b11460cdc57d380
Andrew <andrew@ampletorque.com>
2018-04-14 02:04:38 +0000

    basic connect app

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const app = require('connect')();
-app.use((req, res, next) => {
-	res.end('Hello, world!');
-});
-app.listen(3000);
+const connect = require('connect');
+function logger(req, res, next) {
+	console.log('%s %s', req.method, req.url);
+	next();
+}
+function hello(req, res) {
+	res.setHeader('Content-Type', 'text/plain');
+	res.end('hello world');
+}
+connect()
+	.use(logger)
+	.use(hello)
+	.listen(3000);
 
